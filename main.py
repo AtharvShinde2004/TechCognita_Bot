a3ba617eae325e2746d6aa5718a9c28a4f8bd922
Atharv Shinde <109618836+AtharvShinde2004@users.noreply.github.com>
2024-03-26 01:08:09 +0530

    Updates

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
-# import config
 import os
-
 import discord
 from discord.ext import commands
 import dotenv
+
 dotenv.load_dotenv()
 Token = os.getenv("Token")
 
 EXTENSIONS = [
-    "cogs.events", "cogs.tools", "cogs.moderation", "cogs.owner", "cogs.fun",
-    "cogs.General", "cogs.tech_news", "cogs.utility"]
+    "cogs.events",
+    "cogs.tools",
+    "cogs.moderation",
+    "cogs.owner",
+    "cogs.fun",
+    "cogs.General",
+    "cogs.tech_news",
+    "cogs.utility",
+]
 
 
 class MyBot(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix="!!",
-                         intents=discord.Intents.all(),
-                         strip_after_prefix=True,
-                         case_insensitive=True,
-                         help_command=None,
-                         owner_ids=[554213683102744576],
-                         status=discord.Status.dnd,
-                         activity=discord.Activity(
-                             type=discord.ActivityType.listening,
-                             name="!!help"))
+        super().__init__(
+            command_prefix="!!",
+            intents=discord.Intents.all(),
+            strip_after_prefix=True,
+            case_insensitive=True,
+            help_command=None,
+            owner_ids=[554213683102744576],
+            status=discord.Status.dnd,
+            activity=discord.Activity(
+                type=discord.ActivityType.listening, name="!!help"
+            ),
+        )
+
 
 async def setup_hook(self):
     for file in EXTENSIONS:
@@ -31,11 +40,14 @@ async def setup_hook(self):
         print(file, "activated!")
     await self.tree.sync()
 
+
 async def on_ready(self):
-    print(f"""Logged in as: {self.user}
+    print(
+        f"""Logged in as: {self.user}
     ID: {self.user.id}
     Guilds: {len(self.guilds)}
-    Users: {len(self.users)}""")
+    Users: {len(self.users)}"""
+    )
 
 
 client = MyBot()
